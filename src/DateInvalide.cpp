275ecaed23a5dee7a3cd492b6f7cefebeb42216c
Luca Serban <lucaserban441@gmail.com>
2025-05-26 00:34:24 +0300

    Bug fixes 2.2

--- src/DateInvalide.cpp
+++ src/DateInvalide.cpp
@@ -3,6 +3,9 @@
 //
 #include "DateInvalide.h"
 
-const char* DateInvalide::what() const noexcept {
-    return "Date de intrare invalide! Verifica datele introduse si incearca din nou.";
-}
+class DateInvalide : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Date invalide: valoarea trebuie să fie pozitivă.";
+    }
+};
